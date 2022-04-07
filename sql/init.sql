c6d514ad690c87c24d66ef43f9c24664b85ca8b6
Eragon <eragon@eragon.re>
2022-04-07 12:25:09 +0200

    Fix SQL

--- sql/init.sql
+++ sql/init.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE videgrenierenligne;
 USE videgrenierenligne;
 
-CREATE USER 'webapplication' IDENTIFIED BY '653rag9T';
+CREATE USER "webapplication" IDENTIFIED BY "653rag9T";
 
-GRANT ALL ON DATABASE 'videgrenierenligne' TO 'webapplication';
+GRANT ALL ON DATABASE "videgrenierenligne" TO "webapplication";
 
 SOURCE ./import.sql

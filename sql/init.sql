6c6fbacbca2d08b9e676d0b1e5a3547bb9061028
Eragon <eragon@eragon.re>
2022-05-05 11:15:53 +0200

    Restart

--- sql/init.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS videgrenierenligne;
-CREATE DATABASE videgrenierenligne;
-USE videgrenierenligne;
-
-CREATE USER webapplication@localhost IDENTIFIED BY "653rag9T";
-
-GRANT ALL ON DATABASE videgrenierenligne.* TO webapplication@localhost;
-
-SOURCE ./import.sql
